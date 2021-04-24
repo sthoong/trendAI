cb1b4f89eab12ba6edc5ced216333cbc2c150c9a
Seng Tat Hoong <sthoong@gmail.com>
2021-04-24 18:25:14 +0900

    Tat: update client.html and view.py to get data from CSV

--- NomuWeb/nomusite/webpage/views.py
+++ NomuWeb/nomusite/webpage/views.py
@@ -6,24 +6,54 @@ import pandas as pd
 from django.contrib.staticfiles.storage import staticfiles_storage
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Create your views here.
 def welcome(request):
+    csvFile = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/client_welcome.csv'))
+    clientData = loadCsv(csvFile)
+    clientName = clientData.Client.to_list()
+    clientNotional = clientData.Notional.to_list()
+    clientShares = clientData.Shares.to_list()
+    csvFile = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/market_welcome.csv'))
+    marketData = loadCsv(csvFile)
+    marketName = marketData.Market.to_list()
+    marketNotional = marketData.Notional.to_list()
+    marketShares = marketData.Shares.to_list()
+    csvFile = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/sector_welcome.csv'))
+    sectorData = loadCsv(csvFile)
+    sectorName = sectorData.Sector.to_list()
+    sectorNotional = sectorData.Notional.to_list()
+    sectorShares = sectorData.Shares.to_list()
     return render(request, "webpage/welcome.html", {
-        'userId': "618586"
+        'userId': "618586",
+        'clientName': clientName,
+        'clientNotional': clientNotional,
+        'clientShares': clientShares,
+        'marketName': marketName,
+        'marketNotional': marketNotional,
+        'marketShares': marketShares,
+        'sectorName': sectorName,
+        'sectorNotional': sectorNotional,
+        'sectorShares': sectorShares,
     })
 
 
 def clientReport(request, id):
-    print(request)
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    csvFile = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/'+id+'.csv'))
-    data = loadCsv(csvFile, colName=["Sector","Ticker","Title","Rating","TP"])
+
+    csvFile = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/'+id+'_RecommendedItems.csv'))
+    data = loadCsv(csvFile)
     sectorList = data.Sector.to_list()
     tickerList = data.Ticker.to_list()
     reportList = data.Title.to_list()
     ratingList = data.Rating.to_list()
     priceList = data.TP.to_list()
+
+    similarClient = staticfiles_storage.path(os.path.join(BASE_DIR, 'static/csv/'+id+'_SimilarClientItems.csv'))
+    data = loadCsv(similarClient)
+    similarClientList = data.ClientName.to_list()
+    similarTickerList = data.Ticker.to_list()
+    similarReportList = data.Title.to_list()
 
     print(tickerList)
     return render(request, "webpage/client.html", {
@@ -32,7 +62,10 @@ def clientReport(request, id):
         'tickerList': tickerList,
         'reportList': reportList,
         'ratingList': ratingList,
-        'priceList': priceList
+        'priceList': priceList,
+        'similarClientList': similarClientList,
+        'similarTickerList': similarTickerList,
+        'similarReportList': similarReportList
     })
 
 
@@ -49,6 +82,6 @@ def marketReport(request, id):
 
 
 # Internal Utilities functions
-def loadCsv(csvFile, colName):
+def loadCsv(csvFile):
     return pd.read_csv(csvFile)
 
